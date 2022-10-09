239c3935ca876b6191aff5804018986240c46aeb
bodacom <pischanuk.oik@gmail.com>
2022-10-09 23:45:26 +0300

    Some tests to help Yuriy

--- problems/biks_list.py
+++ problems/biks_list.py
@@ -1,8 +1,16 @@
-list_a = list(range(1,10+1))
+# list_a = list(range(1,10+1))
+# new_list = []
+# print(type(new_list))
+# for number in list_a:
+#     squared_number = number**2
+#     new_list.append(squared_number)
+# print(list_a)
+# print(new_list)
+
+a = ["d", "e", "f"]
+b = ["g", "i", "j"]
 new_list = []
-print(type(new_list))
-for number in list_a:
-    squared_number = number**2
-    new_list.append(squared_number)
-print(list_a)
+for i in a:
+    for j in b:
+        new_list.append(tuple([i, j]))
 print(new_list)
