b699389ba30f43cd063c54e357aa255c155dffb7
bodacom <pischanuk.oik@gmail.com>
2022-10-28 18:44:39 +0300

    Time Tracker main minor code cleanup

--- problems/time_tracker/main.py
+++ problems/time_tracker/main.py
@@ -1,41 +1,3 @@
-# legacy code section
-
-# import Xlib
-# import Xlib.display
-
-# for i in range(20):
-
-#     disp = Xlib.display.Display()
-#     window = disp.get_input_focus().focus
-
-#     # Get active window class and name
-#     print(window.get_wm_class())
-#     print(window.get_wm_name())
-#     time.sleep(1)
-
-# from subprocess import PIPE, Popen
-
-# def get_active_window_title():
-
-#     root = Popen( ['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout = PIPE )
-#     stdout, stderr = root.communicate()
-
-#     m = re.search( b'^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout )
-
-#     if m is not None:
-#         window_id = m.group( 1 )
-#         window = Popen( ['xprop', '-id', window_id, 'WM_NAME'], stdout = PIPE )
-#         stdout, stderr = window.communicate()
-
-#         match = re.match( b'WM_NAME\(\w+\) = (?P<name>.+)$', stdout )
-#         if match is not None:
-#             return match.group( 'name' ).decode( 'UTF-8' ).strip( '"' )
-
-#     return 'Active window not found'
-
-# if __name__ == '__main__':
-#     print( get_active_window_title() )
-
 import time
 import os
 import re
@@ -43,8 +5,8 @@ import sys
 from datetime import datetime
 
 # getwindowfocus == getactivewindow
-
 # 'xdotool getwindowfocus getwindowname getactivewindow getmouselocation'
+
 xdo_command = 'xdotool getactivewindow getwindowname getmouselocation '
 data = ''
 file_name = 'tracker_log.txt'
@@ -66,4 +28,3 @@ while True:
         print('Eror writing to file. Data will be collected in buffer and written while write operation will be available.')
 
     time.sleep(1)
-
